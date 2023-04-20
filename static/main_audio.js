05edb1ac2180a9c64fd94c0ae39da111e7fbdb7c
Nuutti <nuutti.nyyssonen@gmail.com>
2023-04-20 22:50:40 +0200

    added timer and duration

--- static/main_audio.js
+++ static/main_audio.js
@@ -5,7 +5,10 @@ let track = document.getElementById('track');
 let prev = document.getElementById('prev');
 let volume = document.getElementById('volume');
 let next = document.getElementById('forward');
-
+let timer = document.getElementById('duration');
+let currentTimer = document.getElementById('current-time');
+let trackName = document.getElementById('track-name');
+let timeSlider = document.getElementById('seek-slider');
 
 play.addEventListener('click', playAudio)
 prev.addEventListener('click', playAgain)
@@ -24,38 +27,70 @@ function setSong() {
 }
 setSong()
 
+let timeSecond = 0
+
 function playAudio() {
     track.play()
-    console.log(songArray.indexOf('track.src'))
     play.removeEventListener('click', playAudio)
     play.addEventListener('click', pauseAudio)
+    countdown = setInterval(() => {
+        timeSecond++
+        seconds = Math.round(track.duration % 60)
+        timer.textContent = Math.round(track.duration / 60) + ":" + seconds
+        displayTime(timeSecond)
+    }, 1000)
 }
+
+function displayTime(seconds) {
+    sec = Math.floor(seconds % 60)
+    min = Math.floor(seconds / 60)
+    currentTimer.innerHTML = `${min<10 ? "0" : ""}${min}:${sec<10 ? "0" : ""}${sec}`
+}
+
 
 function pauseAudio() {
     track.pause()
     play.removeEventListener('click', pauseAudio)
     play.addEventListener('click', playAudio)
+    clearInterval(countdown)
 }
 
 function playAgain() {
     track.currentTime = 0
-    track.play()
+    clearInterval(countdown)
+    playAudio()
+    timeSecond = 0
+    displayTime(timeSecond)
 }
 
 function prevTrack() {
+    timeRemainder = Math.round(track.duration % 60)
+    timer.textContent = Math.round(track.duration / 60) + ":" + timeRemainder
     track.currentTime = 0
     songIndex -= 1
     track.src = songArray[songIndex]
-    track.play()
+    clearInterval(countdown)
+    playAudio()
+    timeSecond = 0
+    displayTime(timeSecond)
 }
 
 function nextTrack() {
+    timeRemainder = Math.round(track.duration % 60)
+    timer.textContent = Math.round(track.duration / 60) + ":" + timeRemainder
     track.currentTime = 0
     songIndex += 1
     track.src = songArray[songIndex]
-    track.play()
+    clearInterval(countdown)
+    playAudio()
+    timeSecond = 0
+    displayTime(timeSecond)
 }
 
 volume.addEventListener('input', (e) => {
     track.volume = e.currentTarget.value / 100
+})
+
+timeSlider.addEventListener('input', (e) => {
+    track.duration = e.currentTarget.value
 })
